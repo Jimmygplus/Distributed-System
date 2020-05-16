a04a85fa3e89f88bbe6751e9652219ea74fc7422
Jimmy <jimmygplus@foxmail.com>
2020-05-16 18:22:54 +0800

    新目录

---
+++ RMIwarehouseKit/src/Server/Server.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package Server;
+
+/**
+ * @author Jimmy
+ *
+ */
+public class Server {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	startRMIRegistry(): void
+	registerEmployee(String[]): void
+	registerListener(String[]): void
+	offline(String): void
+	sendToAll(String): void
+	sendPrivate(String, int[]): void
+	getEmployeeList(): String[]
+	updateEmployeeList(): void
+	updateChattingList(): void
+	getMaterial(): int
+	getProsyct(): void
+	loadHistory(): void
+	updateHistory(): void
+	changeMaterial(int): void
+	changeProduct(int): void
+
+}
